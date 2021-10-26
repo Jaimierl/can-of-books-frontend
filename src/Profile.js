d47fd84fe02140b89e2a04c2e4ed8746633989d5
Jaimierl <jaimieloney@gmail.com>
2021-10-25 23:43:26 -0400

    updates from class inc. several conditionals. Form outstanding

--- src/Profile.js
+++ src/Profile.js
@@ -1,19 +1,21 @@
 import { Component } from "react";
+import { BrowserRouter as Route } from "react-router-dom";
+import BestBooks from './BestBooks';
 
 class Profile extends Component {
 
   render() {
-    <>
-      {/* /* TODO: render information about logged in user */
-        /* STRETCH TODO: if no logged in user then redirect home */
-        // return <p>Profile page coming soon</p> */}
-        <Route exact path="/">
-          <BestBooks />
-        </Route>
+    return (
+      <>
 
-      
-    </>
+      </>
+    )
   }
 };
 
 export default Profile;
+
+
+// TODO: render information about logged in user 
+// STRETCH TODO: if no logged in user then redirect home
+//return <p>Profile page coming soon</p> 

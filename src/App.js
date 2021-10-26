08da910377c3b3365821fc9aff7fa01f9d4f0119
Bionca Bond <bioncab@zillowgroup.com>
2021-10-25 18:15:38 -0700

    shooter-2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import BestBooks from './BestBooks'; 
+import Profile from './Profile'; 
+
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -36,9 +40,13 @@ class App extends React.Component {
           <Header user={this.state.user} onLogout={this.logoutHandler} />
           <Switch>
             <Route exact path="/">
+                <BestBooks/>
               {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
             </Route>
-            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route exact path="/profile"> 
+                <Profile/>
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+            </Route>
           </Switch>
           <Footer />
         </Router>
--- src/Profile.js
+++ src/Profile.js
@@ -3,9 +3,16 @@ import { Component } from "react";
 class Profile extends Component {
 
   render() {
-    /* TODO: render information about logged in user */
-    /* STRETCH TODO: if no logged in user then redirect home */
-    return <p>Profile page coming soon</p>
+    <>
+      {/* /* TODO: render information about logged in user */
+        /* STRETCH TODO: if no logged in user then redirect home */
+        // return <p>Profile page coming soon</p> */}
+        <Route exact path="/">
+          <BestBooks />
+        </Route>
+
+      
+    </>
   }
 };
 
